34487ef9d5802eec08f56b376720695854931542
Mahathee Chetlapalli <mchetlap@ucsd.edu>
2014-02-21 05:27:08 +0000

    Back Button formatting

--- routes/place.js
+++ routes/place.js
@@ -4,11 +4,11 @@ var data = require('../data.json');
 exports.view = function(req, res){
 	//console.log(data);
 
-	var filterData = {locations: []};
+	var filterData = {category: req.params.category, locations: []};
 	//filter out all the regions
 	if(req.params.category){
 		console.log("category is " + req.params.category);
-
+		console.log(filterData);
 		var locationArr = data.locations;
 		for (var i = 0; i < locationArr.length; i++) {
 		   	if(locationArr[i].category.toUpperCase() === req.params.category.toUpperCase()){
